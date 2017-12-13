069b5a0e06d0266f30c34671613ffba79981e82a
venkat50 <venkatakrishnan.padmanabhan@gmail.com>
2017-12-13 00:15:44 -0500

    Update app.js
    Introduce failure to result in build failure

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.post('/creditscoreV2', creditscore.score);
+app.post('/creditscoreV3', creditscore.score);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
