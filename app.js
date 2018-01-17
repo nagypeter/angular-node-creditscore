9e147d12796951551ccad2e2cd51417d5b063982
Kumar Gaurav <k10gaurav@gmail.com>
2018-01-17 13:31:51 +0530

    Change rest endpoint
    Change rest endpoint

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.post('/creditscore', creditscore.score);
+app.post('/creditscoreV3', creditscore.score);
 
 
 http.createServer(app).listen(app.get('port'), function(){
